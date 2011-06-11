79ca0489ea826f45d5b288e71f1b92ca8b91d914
Jakub Bartkowiak <gralinpl@gmail.com>
2011-06-11 19:16:42 +0000

    removed unused reference in SampleApp prepared to add netduino board in future changed the SampleApp: * in addition to token, a delay value is passed, so that every device knows how long to wait before sending token back (led turn on) * the delay value is updated each round by Domino (it reads the variable resistor)
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS30]$/nrf24l01;C2961

--- SampleApp/SampleApp.cs
+++ SampleApp/SampleApp.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text;
 using System.Threading;
+using GHIElectronics.NETMF.Hardware;
 using Microsoft.SPOT;
 using Microsoft.SPOT.Hardware;
 
@@ -17,45 +18,56 @@ namespace Gralin.NETMF.Nordic
         private readonly SPI.SPI_module _spi;
         private Timer _timer;
         private DateTime _lastActivity;
-        private byte _currentValue;
+        private byte _token;
+        private byte _delay;
         private byte[] _myAddress;
-        private byte[][] _otherFez;
+        private byte[][] _otherBoards;
+        private AnalogIn _variableResistor;
 
         public SampleApp()
         {
-            Debug.EnableGCMessages(false);
+            Debug.EnableGCMessages(true);
 
-            switch ((Fez) SystemInfo.SystemID.Model)
+            switch (SystemInfo.OEMString)
             {
-                case Fez.Mini:
-                    _chipSelectPin = (Cpu.Pin) 11; // FEZ_Pin.Digital.UEXT5
-                    _interruptPin = (Cpu.Pin) 43;  // FEZ_Pin.Interrupt.UEXT10
-                    _chipEnablePin = (Cpu.Pin) 1;  // FEZ_Pin.Digital.UEXT6
-                    _spi = SPI.SPI_module.SPI1;
-                    _led = new OutputPort((Cpu.Pin) 4, false);
+                case "GHI Electronics, LLC":
+                    switch ((Fez)SystemInfo.SystemID.Model)
+                    {
+                        case Fez.Mini:
+                            _chipSelectPin = (Cpu.Pin)11; // FEZ_Pin.Digital.UEXT5
+                            _interruptPin = (Cpu.Pin)43;  // FEZ_Pin.Interrupt.UEXT10
+                            _chipEnablePin = (Cpu.Pin)1;  // FEZ_Pin.Digital.UEXT6
+                            _spi = SPI.SPI_module.SPI1;
+                            _led = new OutputPort((Cpu.Pin)28, false); // FEZ_Pin.Digital.An0
+                            break;
+
+                        case Fez.Domino:
+                            _chipSelectPin = (Cpu.Pin)11; // FEZ_Pin.Digital.UEXT5
+                            _interruptPin = (Cpu.Pin)12;  // FEZ_Pin.Interrupt.UEXT10
+                            _chipEnablePin = (Cpu.Pin)1;  // FEZ_Pin.Digital.UEXT6
+                            _spi = SPI.SPI_module.SPI2;
+                            _led = new OutputPort((Cpu.Pin)20, false); // FEZ_Pin.Digital.Di0
+                            break;
+
+                        case Fez.Cobra:
+                            _chipSelectPin = (Cpu.Pin)75; // FEZ_Pin.Digital.UEXT5
+                            _interruptPin = (Cpu.Pin)26;  // FEZ_Pin.Interrupt.UEXT10
+                            _chipEnablePin = (Cpu.Pin)48; // FEZ_Pin.Digital.UEXT6
+                            _spi = SPI.SPI_module.SPI2;
+                            _led = new OutputPort((Cpu.Pin)46, false); // FEZ_Pin.Digital.IO46
+                            break;
+
+                        default:
+                            throw new NotSupportedException("Unknown board!");
+                    }
                     break;
 
-                case Fez.Domino:
-                    _chipSelectPin = (Cpu.Pin) 11; // FEZ_Pin.Digital.UEXT5
-                    _interruptPin = (Cpu.Pin) 12;  // FEZ_Pin.Interrupt.UEXT10
-                    _chipEnablePin = (Cpu.Pin) 1;  // FEZ_Pin.Digital.UEXT6
-                    _spi = SPI.SPI_module.SPI2;
-                    _led = new OutputPort((Cpu.Pin) 4, false);
-                    break;
-
-                case Fez.Cobra:
-                    _chipSelectPin = (Cpu.Pin) 75; // FEZ_Pin.Digital.UEXT5
-                    _interruptPin = (Cpu.Pin) 26;  // FEZ_Pin.Interrupt.UEXT10
-                    _chipEnablePin = (Cpu.Pin) 48; // FEZ_Pin.Digital.UEXT6
-                    _spi = SPI.SPI_module.SPI2;
-
-                    // IO46 pin is used instead of Cobra onboard led
-                    // because the led is connected in parallel with chipEnablePin
-                    _led = new OutputPort((Cpu.Pin) 46, false);
-                    break;
+                case "Netduino Mini by Secret Labs LLC":
+                    // this example will be extended to Netduino family
+                    throw new NotSupportedException("Netduino mini is not yet supported!");
 
                 default:
-                    throw new NotSupportedException("Need to add SPI configuration for this device");
+                    throw new NotSupportedException("Unknown board!");
             }
 
             _module = new NRF24L01Plus();
@@ -67,35 +79,42 @@ namespace Gralin.NETMF.Nordic
             const byte channel = 10;
 
             // all addresses need to have the same length
-            var dominoAddress = Encoding.UTF8.GetBytes("DOMIN");
-            var miniAddress   = Encoding.UTF8.GetBytes("MINI.");
-            var cobraAddress  = Encoding.UTF8.GetBytes("COBRA");
+            var fezDominoAddress = Encoding.UTF8.GetBytes("DOMIN");
+            var fezMiniAddress   = Encoding.UTF8.GetBytes("MINI.");
+            var fezCobraAddress  = Encoding.UTF8.GetBytes("COBRA");
 
             // here we determine on which device the code is running on
-            switch ((Fez) SystemInfo.SystemID.Model)
+            switch (SystemInfo.OEMString)
             {
-                case Fez.Mini:
-                    _myAddress = miniAddress;
-                    _otherFez = new byte[2][];
-                    _otherFez[0] = dominoAddress;
-                    _otherFez[1] = cobraAddress;
-                    break;
+                case "GHI Electronics, LLC":
+                    switch ((Fez)SystemInfo.SystemID.Model)
+                    {
+                        case Fez.Mini:
+                            _myAddress = fezMiniAddress;
+                            _otherBoards = new byte[2][];
+                            _otherBoards[0] = fezDominoAddress;
+                            _otherBoards[1] = fezCobraAddress;
+                            break;
 
-                case Fez.Domino:
-                    _myAddress = dominoAddress;
-                    _otherFez = new byte[2][];
-                    _otherFez[0] = cobraAddress;
-                    _otherFez[1] = miniAddress;
-                    break;
+                        case Fez.Domino:
+                            _myAddress = fezDominoAddress;
+                            _otherBoards = new byte[2][];
+                            _otherBoards[0] = fezCobraAddress;
+                            _otherBoards[1] = fezMiniAddress;
+                            _variableResistor = new AnalogIn(AnalogIn.Pin.Ain0);
+                            _variableResistor.SetLinearScale(0,3300);
+                            break;
 
-                case Fez.Cobra:
-                    _myAddress = cobraAddress;
-                    _otherFez = new byte[2][];
-                    _otherFez[0] = miniAddress;
-                    _otherFez[1] = dominoAddress;
+                        case Fez.Cobra:
+                            _myAddress = fezCobraAddress;
+                            _otherBoards = new byte[2][];
+                            _otherBoards[0] = fezMiniAddress;
+                            _otherBoards[1] = fezDominoAddress;
+                            break;
+                    }
                     break;
             }
-
+            
             // here we attatch event listener
             _module.OnDataReceived += OnReceive;
             _module.OnTransmitFailed += OnSendFailure;
@@ -108,18 +127,17 @@ namespace Gralin.NETMF.Nordic
             // to start receiveing we need to call Enable(), call Disable() to stop/pause
             _module.Enable();
 
-
             // example of reading your own address
             var myAddress = _module.GetAddress(AddressSlot.Zero, 5);
             Debug.Print("I am " + new string(Encoding.UTF8.GetChars(myAddress)));
 
             _lastActivity = DateTime.MinValue;
 
-            // Domino board is the one that starts the token passing and monitors if a token was lost
+            // Fez Domino board is the one that starts the token passing and monitors if a token was lost
             // The timer checks each 10 sec if any token has been received since last check
             // If not than it might mean that a token was lost or never send - a new is created
             // A token may be lost if a board is reseted before sending token back
-            if ((Fez) SystemInfo.SystemID.Model == Fez.Domino)
+            if ((Fez)SystemInfo.SystemID.Model == Fez.Domino)
             {
                 _timer = new Timer(CreateToken, null, new TimeSpan(0, 0, 0, 10), new TimeSpan(0, 0, 0, 10));
             }
@@ -129,7 +147,17 @@ namespace Gralin.NETMF.Nordic
         {
             if (DateTime.Now.Subtract(_lastActivity).Seconds < 10) return;
             _lastActivity = DateTime.Now;
-            SendTokenToFez();
+            _token = 0;
+            Send();
+        }
+
+        private void UpdateDelay()
+        {
+            // only Cobra has a variable resistor connected
+            if ((Fez)SystemInfo.SystemID.Model == Fez.Domino)
+            {
+                _delay = (byte)(((_variableResistor.Read() - 1100) / 2200.0) * 255);
+            }
         }
 
         private void OnSendSuccess()
@@ -139,42 +167,50 @@ namespace Gralin.NETMF.Nordic
 
         private void OnSendFailure()
         {
-            SendTokenToFez();
+            Debug.Print("Send failed!");
+            Send();
         }
 
         private void OnReceive(byte[] data)
         {
-            Debug.Print(data[0].ToString());
+            Debug.Print("Token = " + data[0] + ", Delay = " + data[1]);
             _lastActivity = DateTime.Now;
-            _currentValue = (byte) (data[0] + 1);
-            SendTokenToFez();
+            _token = (byte) (data[0] + 1);
+            _delay = data[1];
+            Send();
         }
 
-        private void SendTokenToFez()
+        private void Send()
         {
-            //SendTokenToRandomFez();
-            SendTokenToNextFez();
+            // update the delay value by reading the varaible resistor
+            UpdateDelay();
+
+            // uncomment this line for a random passing version
+            //SendTokenToRandomBoard();
+
+            SendTokenToNextBoard();
         }
 
-        private void SendTokenToRandomFez()
+        private void SendTokenToRandomBoard()
         {
             var nextIndex = _rand.Next(2);
             _led.Write(true);
 
             // delay added to see LED blink
-            Thread.Sleep(1000);
+            Thread.Sleep(_delay*2 + 100);
 
-            _module.SendTo(_otherFez[nextIndex], new[] { _currentValue });
+            _module.SendTo(_otherBoards[nextIndex], new[] { _token });
         }
 
-        private void SendTokenToNextFez()
+        private void SendTokenToNextBoard()
         {
             _led.Write(true);
 
             // delay added to see LED blink
-            Thread.Sleep(1000);
+            // the _delay value is 0...255
+            Thread.Sleep(_delay*4 + 20);
 
-            _module.SendTo(_otherFez[0], new[] { _currentValue });
+            _module.SendTo(_otherBoards[0], new[] { _token, _delay });
         }
     }
 }
