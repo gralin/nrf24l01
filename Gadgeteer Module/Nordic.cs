d355c8e3961ac784111f0aa0d33041eb2724be6d
Jakub Bartkowiak <gralinpl@gmail.com>
2012-04-02 07:46:21 +0000

    Added Gadgeteer module project
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS30]$/nrf24l01;C13614

---
+++ Gadgeteer Module/Nordic.cs
@@ -0,0 +1,28 @@
+﻿using Gralin.NETMF.Nordic;
+using GTM = Gadgeteer.Modules;
+
+namespace Gadgeteer.Modules.Gralin
+{
+    /// <summary>
+    /// A Nordic module for Microsoft .NET Gadgeteer
+    /// </summary>
+    public class Nordic : Module
+    {
+        /// <summary>
+        /// Module API.
+        /// </summary>
+        public NRF24L01Plus Api { get; protected set; }
+
+        /// <summary>
+        /// Gadgeteer Nordic NRF24L01+ module.
+        /// </summary>
+        /// <param name="socketNumber">The socket that this module is plugged in to.</param>
+        public Nordic(int socketNumber)
+        {
+            var socket = Socket.GetSocket(socketNumber, true, this, null);
+
+            Api = new NRF24L01Plus();
+            Api.Initialize(socket.SPIModule, socket.CpuPins[6], socket.CpuPins[4],socket.CpuPins[3]);
+        }
+    }
+}
